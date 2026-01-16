0130ccdd2092cf197b6fd97cedbc077493101f75
indeviral <mb@ind.by>
2026-01-16 21:10:00 +0300

    change bootstrap 2

--- src/js/pybbjs.js
+++ src/js/pybbjs.js
@@ -1,3 +1,12 @@
+var _____WB$wombat$assign$function_____=function(name){return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name))||self[name];};if(!self.__WB_pmw){self.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opens = _____WB$wombat$assign$function_____("opens");
 function pybb_delete_post(url, post_id, confirm_text) {
     if (!confirm(confirm_text))
         return false;
@@ -179,3 +188,25 @@ jQuery(function ($) {
         });
     }
 });
+
+}
+/*
+     FILE ARCHIVED ON 05:44:08 May 28, 2020 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 18:05:58 Jan 16, 2026.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.854
+  exclusion.robots: 0.069
+  exclusion.robots.policy: 0.054
+  esindex: 0.015
+  cdx.remote: 40.008
+  LoadShardBlock: 230.085 (3)
+  PetaboxLoader3.datanode: 146.761 (4)
+  PetaboxLoader3.resolve: 86.321 (2)
+  load_resource: 61.551
+*/
